f8247bb0efb65f650a72e403fab377749e3dbd5a
mozzaru <rianyanuar52@gmail.com>
2025-05-19 23:17:45 +0700

    Ainzscans: try bypass cf

--- src/id/ainzscansid/src/eu/kanade/tachiyomi/extension/id/ainzscansid/AinzScansID.kt
+++ src/id/ainzscansid/src/eu/kanade/tachiyomi/extension/id/ainzscansid/AinzScansID.kt
@@ -1,8 +1,16 @@
 package eu.kanade.tachiyomi.extension.id.ainzscansid
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import okhttp3.OkHttpClient
 
-class AinzScansID : MangaThemesia("Ainz Scans ID", "https://ainzscans.net", "id", "/series") {
-
+class AinzScansID : MangaThemesia(
+    "Ainz Scans ID",
+    "https://ainzscans.net",
+    "id",
+    "/series"
+) {
     override val hasProjectPage = true
+
+    // Gunakan client dengan CloudflareInterceptor
+    override val client: OkHttpClient = network.cloudflareClient
 }
