474857d6a0c0bf9de167d0a655c5725ba6749305
mozzaru <rianyanuar52@gmail.com>
2025-07-20 11:26:55 +0700

    mgkomik: new code

--- src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
+++ src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
@@ -1,20 +1,13 @@
 package eu.kanade.tachiyomi.extension.id.mgkomik
 
-import android.app.Application
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.Filter
-import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.SFilter
-import eu.kanade.tachiyomi.source.model.SUriFilter
+import okhttp3.Headers
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.jsoup.nodes.Document
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 class MGKomik : Madara(
     "MG Komik",
@@ -23,81 +16,39 @@ class MGKomik : Madara(
     SimpleDateFormat("dd MMM yy", Locale.US),
 ) {
     override val useLoadMoreRequest = LoadMoreStrategy.Never
+
     override val useNewChapterEndpoint = false
-    override val mangaSubString = "komik"
 
-    override val client: OkHttpClient = CloudflareBypassHelper.wrapClientWithBypass(
-        context = Injekt.get<Application>(),
-        base = network.cloudflareClient.newBuilder()
-            .rateLimit(9, 2)
-            .build(),
-    )
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(20, 5, TimeUnit.SECONDS)
+        .build()
 
-    // ============================ Popular ============================
-    override fun popularMangaNextPageSelector() = ".wp-pagenavi span.current + a"
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder()
+        .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+        .add("Accept-Language", "en-US,en;q=0.9,id;q=0.8")
+        .add("Sec-Fetch-Dest", "document")
+        .add("Sec-Fetch-Mode", "navigate")
+        .add("Sec-Fetch-Site", "same-origin")
+        .add("Sec-Fetch-User", "?1")
+        .add("Upgrade-Insecure-Requests", "1")
+        .add("X-Requested-With", randomString)
 
-    // ============================ Latest =============================
-    override fun latestUpdatesRequest(page: Int): Request =
-        GET("$baseUrl/$mangaSubString/${searchPage(page)}", headers)
-
-    // ============================ Search =============================
-    override fun searchRequest(page: Int, query: String, filters: FilterList): Request {
-        filters.filterIsInstance<GenreContentFilter>().forEach { filter ->
-            val url = filter.toUriPart()
-            if (url.isNotBlank()) {
-                return GET(url, headers)
-            }
-        }
-        return super.searchRequest(page, query, filters)
+    private fun generateRandomString(length: Int): String {
+        val charset = "HALOGaES.BCDFHIJKMNPQRTUVWXYZ.bcdefghijklmnopqrstuvwxyz0123456789"
+        return (1..length)
+            .map { charset.random() }
+            .joinToString("")
     }
 
-    override fun searchMangaSelector() =
-        "${super.searchMangaSelector()}, .page-listing-item .page-item-detail"
+    override fun searchPage(page: Int): String = if (page > 1) "page/$page/" else ""
+
+    private val randomLength = Random.Default.nextInt(13, 21)
+
+    private val randomString = generateRandomString(randomLength)
+
+    override val mangaSubString = "komik"
 
     override fun searchMangaNextPageSelector() = "a.page.larger"
 
-    // ============================ Chapters ============================
     override val chapterUrlSuffix = ""
-
-    // ============================ Filters =============================
-    override fun getFilterList(): FilterList {
-        launchIO { fetchGenres() }
-
-        val filters = super.getFilterList().list.toMutableList()
-
-        filters += if (genresList.isNotEmpty()) {
-            listOf(
-                Filter.Separator(),
-                GenreContentFilter(
-                    title = intl["genre_filter_title"],
-                    options = genresList.map { it.name to it.id },
-                ),
-            )
-        } else {
-            listOf(
-                Filter.Separator(),
-                Filter.Header(intl["genre_missing_warning"]),
-            )
-        }
-
-        return FilterList(filters)
-    }
-
-    private class GenreContentFilter(
-        title: String,
-        options: List<Pair<String, String>>,
-    ) : UriPartFilter(
-        title,
-        options.toTypedArray(),
-    )
-
-    override fun genresRequest(): Request = GET("$baseUrl/$mangaSubString", headers)
-
-    override fun parseGenres(document: Document): List<Genre> {
-        val genres = mutableListOf(Genre("All", ""))
-        genres += document.select(".row.genres li a").map {
-            Genre(it.text(), it.absUrl("href"))
-        }
-        return genres
-    }
 }
