c8f47e9f47ac582c70ade8f72c64bd935f806590
mozzaru <100791931+mozzaru@users.noreply.github.com>
2026-04-18 05:47:04 +0000

    feat(id): add MG Komik Beta extension using NatsuId theme
    - Create new extension for https://web.mgkomik.cc/
    - Use NatsuId multisrc theme as requested to fix list parsing
    - Reuse original MG Komik icons for branding

--- src/id/mgkomikbeta/src/eu/kanade/tachiyomi/extension/id/mgkomikbeta/MGKomikBeta.kt
+++ src/id/mgkomikbeta/src/eu/kanade/tachiyomi/extension/id/mgkomikbeta/MGKomikBeta.kt
@@ -1,14 +1,10 @@
 package eu.kanade.tachiyomi.extension.id.mgkomikbeta
 
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
+import eu.kanade.tachiyomi.multisrc.natsuid.NatsuId
 
 class MGKomikBeta :
-    MangaThemesia(
+    NatsuId(
         "MG Komik Beta",
-        "https://web.mgkomik.cc",
         "id",
-        mangaUrlDirectory = "/komik",
-        dateFormat = SimpleDateFormat("dd MMM yyyy", Locale("id")),
+        "https://web.mgkomik.cc",
     )
